efa9a57763d5c5ac427c1686b86d3359a0430d80
Oscar Garcia <oscar.github@linaresdigital.com>
2015-05-28 20:23:32 +0200

    Fix #1: Renamed scripts and support for command line parameters

--- create2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop ub1404-dev2
-docker rm ub1404-dev2
-docker create --privileged -e "DISPLAY" -v="/dev/nvidiactl:/dev/nvidiactl:rw" -v="/var/run/bumblebee.socket:/var/run/bumblebee.socket:rw" -v="/dev/dri:/dev/dri:rw" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v="/home/redstar:/home/redstar:rw" -u redstar -w /home/redstar -h ub1404-dev2 --name="ub1404-dev2" -i -t ub1404-dev /bin/bash
-
--- start2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xhost si:localuser:redstar
-docker start ub1404-dev2
-
