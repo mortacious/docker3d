c90cf0c2e098acf235825b94e18b02a92852d112
Oscar Garcia <oscar.github@linaresdigital.com>
2015-05-28 20:24:39 +0200

    Fix #1: Enhance script code to allow command line parameters

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#! /bin/bash
 
-docker build -t ub1404-dev .
+# Load image name from command line
+[ -z "$1" ] && IMAGE_NAME="ub1404-dri" || IMAGE_NAME="$1"
+
+echo "Building '$IMAGE_NAME' image"
+docker build -t "$IMAGE_NAME" . && echo -e "Done!\nList available images with 'docker images'" || echo 'Build failed!'
+
